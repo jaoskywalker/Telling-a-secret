376fc67cd2ec0e5b051220564c3db3b26693aa3e
João Martins <john_victor100@hotmail.com>
2022-05-09 19:34:05 -0300

    More improviments

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    
+
     <!-- META TAGS-->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <!-- BOOTSTRAP AND CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
@@ -14,38 +15,131 @@
 
     <title>Telling a Secret</title>
 </head>
+
 <body>
     <header>
         <nav class="navbar">
-        <a class="navbar-brand" href="#">
-        <i class="bi bi-incognito"></i>
-        <span class="mb-0 h1">Telling a Secret</span>
-        </a>
-    </nav>
+            <a class="navbar-brand" href="#">
+                <i class="bi bi-incognito"></i>
+                <span class="mb-0 h1">Telling a Secret</span>
+            </a>
+        </nav>
     </header>
     <main>
+
+        <!-- INPUT FORM ----------------------------------------------------------------------------------------->
         <section class="container">
-        <div class="form-container">
-                <div class="form-group" >
+            <div class="form-container">
+                <div class="form-group">
                     <textarea class="form-control" rows="3" cols="40" placeholder="Write your secret here"></textarea>
-                    <hr>   
-                <div class="row">
-                    <div class="col-4">
-                    <select class="form-control">
-                    <option value="" disabled selected>Select your region</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                    </select>
+                    <hr>
+                    <div class="row">
+                        <div class="col-4">
+                            <select class="form-control">
+                                <option value="" disabled selected>Select your region</option>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                        </div>
+                        <div class="col-8 text-right">
+                            <button type="button" class="btn">Tell us</button>
+                        </div>
                     </div>
-                    <div class="col-8 text-right">
-                    <button type="button" class="btn">Tell us</button>
+                </div>
+        </section>
+        <!-- BADGE DIV --------------------------------------------------------------------------------------------->
+        <section class="selection-posts">
+            <h6>New Posts <span class="badge bg-secondary bg-purple">New</span></h6>
+        </section>
+        <!-- POSTS DIV --------------------------------------------------------------------------------------------->
+        <section class="container">
+            <div class="row">
+                <div class="col-sm-6 card-div">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <div class="row">
+                            <button type="button" class="btn card-btn">
+                                <i class="bi bi-hand-thumbs-up"></i>
+                                Likes <span class="badge badge-dark">9</span>
+                                <span class="sr-only">people liked</span>
+                            </button>
+                            <button type="button" class="btn">
+                                <i class="bi bi-chat-dots"></i>
+                                Comment <span class="badge badge-dark">9</span>
+                                <span class="sr-only">people comments</span>
+                            </button>
+                        </div>
+                        </div>
                     </div>
-                </div> 
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                           <div class="row">
+                            <button type="button" class="btn card-btn">
+                                <i class="bi bi-hand-thumbs-up"></i>
+                                Likes <span class="badge badge-dark">9</span>
+                                <span class="sr-only">people liked</span>
+                            </button>
+                            <button type="button" class="btn">
+                                <i class="bi bi-chat-dots"></i>
+                                Comment <span class="badge badge-dark">9</span>
+                                <span class="sr-only">people comments</span>
+                            </button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                           <div class="row">
+                            <button type="button" class="btn card-btn">
+                                <i class="bi bi-hand-thumbs-up"></i>
+                                Likes <span class="badge badge-dark">9</span>
+                                <span class="sr-only">people liked</span>
+                            </button>
+                            <button type="button" class="btn">
+                                <i class="bi bi-chat-dots"></i>
+                                Comment <span class="badge badge-dark">9</span>
+                                <span class="sr-only">people comments</span>
+                            </button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                           <div class="row">
+                            <button type="button" class="btn card-btn">
+                                <i class="bi bi-hand-thumbs-up"></i>
+                                Likes <span class="badge badge-dark">9</span>
+                                <span class="sr-only">people liked</span>
+                            </button>
+                            <button type="button" class="btn">
+                                <i class="bi bi-chat-dots"></i>
+                                Comment <span class="badge badge-dark">9</span>
+                                <span class="sr-only">people comments</span>
+                            </button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
 </body>
+
 </html>
